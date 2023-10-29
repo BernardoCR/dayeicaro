6782ee89781d9baee13d89b10e1c8561293253ad
BernardoCR <bernardocr@hotmail.com>
2023-10-28 22:02:37 -0300

    Update Minicart.tsx

--- src/components/GiftList/Minicart.tsx
+++ src/components/GiftList/Minicart.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
 
 import IconGift from "../../../public/icon-gift.react.svg";
 import IconTrash from "../../../public/icon-trash.react.svg";
@@ -6,12 +6,12 @@ import IconPlus from "../../../public/icon-plus.react.svg";
 import IconMinus from "../../../public/icon-minus.react.svg";
 import IconX from "../../../public/icon-x.react.svg";
 import { useCart } from "./CartContext";
-
 import PaymentModal from "./PaymentModal";
 
 type MinicartQuantityBadgeProps = {
   itemQuantity: number;
 };
+
 function MinicartQuantityBadge({
   itemQuantity,
 }: MinicartQuantityBadgeProps): JSX.Element {
@@ -25,6 +25,7 @@ function MinicartQuantityBadge({
 type MinicartItemProps = {
   cartItem: CartItem;
 };
+
 function MinicartItem({ cartItem }: MinicartItemProps) {
   const { updateCart, itemTotalAmount } = useCart();
 
@@ -54,7 +55,7 @@ function MinicartItem({ cartItem }: MinicartItemProps) {
             updateCart({ type: "decreaseItemQuantity", item: cartItem.name })
           }
         >
-          <IconMinus className="h-[14px] " />
+          <IconMinus className="h-[14px]" />
         </button>
       </div>
       <div className="flex-grow leading-tight">{cartItem.name}</div>
@@ -72,11 +73,16 @@ type MinicartItemListProps = {
   setPaymentOpen: SetPaymentOpen;
   setItemListOpen: React.Dispatch<React.SetStateAction<boolean>>;
   itemListOpen: boolean;
+  message: string;
+  setMessage: (message: string) => void;
 };
+
 function MinicartItemList({
   setPaymentOpen,
   setItemListOpen,
   itemListOpen,
+  message,
+  setMessage,
 }: MinicartItemListProps): JSX.Element {
   const { cart, cartTotalAmount } = useCart();
 
@@ -103,14 +109,16 @@ function MinicartItemList({
             className="fixed top-0 bottom-0 left-0 right-0 z-30 flex items-center justify-center bg-black bg-opacity-60 text-joanGreen-600 md:hidden"
             onClick={() => setItemListOpen(false)}
           ></div>
-          <div className="fixed bottom-0 left-0 right-0 z-40 animate-fade-in-up  shadow-xl md:static md:mb-4">
+          <div className="fixed bottom-0 left-0 right-0 z-40 animate-fade-in-up shadow-xl md:static md:mb-4">
             <div className="flex flex-col rounded-t-lg border border-joanGreen-600 bg-white text-sm uppercase text-joanGreen-600 md:w-[29rem] md:rounded-md">
-              <button
+
+            <button
                 className="-right-[0.85rem] flex min-h-[2rem] min-w-[2rem] items-center justify-center self-end rounded-full border-joanGreen-600 bg-white p-[0.75rem] hover:bg-joanGreen-50 md:absolute md:-top-[0.85rem] md:border md:p-0"
                 onClick={() => setItemListOpen(false)}
               >
                 <IconX className="h-[1.75rem] md:h-[0.875rem]" />
               </button>
+
               <div className="space-y-4 p-4 pt-0 md:pt-6">
                 <div className="space-y-2">
                   {cart.items.map((cartItem, index) => (
@@ -121,9 +129,19 @@ function MinicartItemList({
                   <span className="mr-4">Total</span>
                   <span>R${cartTotalAmount}</span>
                 </div>
+                <textarea
+                  rows={4}
+                  placeholder="Escreva uma mensagem (máx. 56 caracteres)"
+                  onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+                    setMessage(e.target.value)
+                  }
+                  value={message}
+                  className="w-full p-2 border border-joanGreen-600 rounded-lg my-4"
+                  maxLength={56}
+                />
               </div>
               <button
-                className="h-12 select-none border-t border-joanGreen-600 bg-joanGreen-600 text-base uppercase text-white hover:bg-joanGreen-550 md:rounded-b-sm"
+                className="h-12 select-none border-t border-joanGreen-600 bg-joanGreen-600 text-base uppercase text-white hover-bg-joanGreen-550 md:rounded-b-sm"
                 onClick={() => setPaymentOpen(true)}
               >
                 Pagar agora
@@ -138,23 +156,36 @@ function MinicartItemList({
 
 type MinicartFloatingButtonProps = {
   itemQuantity: number;
+  itemListOpen: boolean;
+  setItemListOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  message: string;
+  setMessage: (message: string) => void;
 };
+
 function MinicartFloatingButton({
   itemQuantity,
+  itemListOpen,
+  setItemListOpen,
+  message,
+  setMessage,
 }: MinicartFloatingButtonProps): JSX.Element {
   return (
-    <div className="relative flex h-16 w-16 items-center justify-center rounded-full border border-joanGreen-600 bg-white text-joanGreen-600 shadow-xl">
+    <button
+      className="fixed bottom-4 right-4 flex h-16 w-16 items-center justify-center rounded-full border border-joanGreen-600 bg-white text-joanGreen-600 shadow-xl"
+      onClick={() => setItemListOpen(true)}
+    >
       {itemQuantity > 0 && (
         <MinicartQuantityBadge itemQuantity={itemQuantity} />
       )}
       <IconGift className="h-[33px]" />
-    </div>
+    </button>
   );
 }
 
 function Minicart(): JSX.Element {
   const [itemListOpen, setItemListOpen] = useState(false);
   const [paymentOpen, setPaymentOpen] = useState(false);
+  const [message, setMessage] = useState("");
 
   const { cart } = useCart();
   const itemQuantity = cart.items.reduce(
@@ -169,28 +200,25 @@ function Minicart(): JSX.Element {
 
   return (
     <>
-      {paymentOpen && <PaymentModal setPaymentOpen={setPaymentOpen} />}
+      {paymentOpen && (
+        <PaymentModal setPaymentOpen={setPaymentOpen} message={message} />
+      )}
       <div className="relative z-40">
         <div className="fixed bottom-4 right-4 flex flex-col items-end md:bottom-6 md:right-6 lg:bottom-12 lg:right-12">
           <MinicartItemList
             setPaymentOpen={setPaymentOpen}
             setItemListOpen={setItemListOpen}
             itemListOpen={itemListOpen}
+            message={message}
+            setMessage={setMessage}
           />
-          {itemQuantity > 0 ? (
-            <button
-              className="rounded-full"
-              onClick={() =>
-                itemListOpen ? setItemListOpen(false) : setItemListOpen(true)
-              }
-            >
-              <MinicartFloatingButton itemQuantity={itemQuantity} />
-            </button>
-          ) : (
-            <a href="#lista-de-presentes" aria-label="Lista de presentes">
-              <MinicartFloatingButton itemQuantity={itemQuantity} />
-            </a>
-          )}
+          <MinicartFloatingButton
+            itemQuantity={itemQuantity}
+            itemListOpen={itemListOpen}
+            setItemListOpen={setItemListOpen}
+            message={message}
+            setMessage={setMessage}
+          />
         </div>
       </div>
     </>
