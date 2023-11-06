3d75a6098c2ed8cb95698295e01edf475bbbffa0
BernardoCR <bernardocr@hotmail.com>
2023-11-06 18:16:08 -0300

    Update index.tsx

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import logo from "../../../public/chandra-e-joao.svg";
+import logo from "../../../public/day-e-icaro.svg";
 
 type Background = "dark" | "light";
 
