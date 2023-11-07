8bcc693e50c4def7e7eaed0ffc6fe2099d678a21
BernardoCR <bernardocr@hotmail.com>
2023-11-07 08:35:12 -0300

    Update index.tsx

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -22,9 +22,10 @@ function Question({ question, answer }: QuestionProps): JSX.Element {
 function FAQ(): JSX.Element {
   return (
     <div className="space-y-16 border-t border-joanGreen-300 px-8 py-10 text-joanGreen-600 selection:bg-joanGreen-600 selection:text-white md:p-6">
-      <div className="text-center font-serif text-4xl">
+      <div className="text-center font-serif text-sm"> <!-- Added text-sm -->
         Baseado no código original de Chandra Drummond (https://github.com/jpesce/joandra-wedding)
       </div>
+
       <div className="m-auto max-w-[52rem] border-b border-joanGreen-600">
         {questionList.map((question, index) => (
           <Question
